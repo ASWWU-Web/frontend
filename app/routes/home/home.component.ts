7cf05ebdfb01a68bab5c00372b2306d15bdb7cac
Nolan Chinn <nolan694@gmail.com>
2017-04-07 16:40:14 -0700

    dummy backend on home component

--- app/routes/home/home.component.ts
+++ app/routes/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,11 @@ import {Component, NgModule} from '@angular/core';
 })
 
 export class HomeComponent {
+	forms: any[] = [
+		{formID: "1", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
+		{formID: "2", name: "chef", img: "http://lorempixel.com/300/200/abstract/", desc: "cookin'"},
+		{formID: "3", name: "web wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "the best job"},
+		{formID: "4", name: "regular wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "another good one"},
+		{formID: "5", name: "president", img: "http://lorempixel.com/300/200/abstract/", desc: "of the US? or just ASWWU?"},
+	];
 }
