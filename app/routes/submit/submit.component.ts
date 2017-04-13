e2c9630ad1616049146ff23f00cc8380a8fb8c5d
Nolan Chinn <nolan694@gmail.com>
2017-04-12 17:18:53 -0700

    build answers array and add 2way data binding

--- app/routes/submit/submit.component.ts
+++ app/routes/submit/submit.component.ts
@@ -1,18 +1,31 @@
-import {Component, NgModule} from '@angular/core';
+import {Component, OnInit, NgModule} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
   selector: 'submit',
   templateUrl: 'app/routes/submit/submit.component.html',
 })
 
-export class SubmitComponent {
+export class SubmitComponent implements OnInit {
 	form: any = { formID: "1", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs", owner: "1", questions: [{ID: "1", text: "What's your favorite color?"}, {ID: "2", text: "What's the best animal?"}]};
-	answers: any[];
+	answers: any[] = [];
 	formID: number;
 
 	constructor(route: ActivatedRoute) {
-		this.formID = +route.snapshot.params['formID'];
+		this.formID = +route.snapshot.params['formID'];	
+	}
+
+	ngOnInit() {
+		//build the empty answers array
+		this.form.questions.forEach((entry) => {
+			let answerObj = {ID: entry.ID, answer: ""};
+			this.answers.push(answerObj);
+		});
+	}
+
+	onSubmit() {
+		console.log(this.answers);
 	}
 
 }
