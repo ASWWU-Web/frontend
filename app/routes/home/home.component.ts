8c17f2da27690c5374f6fa9e90e499a69c05403c
asteroidice <ryan.s.rabello@gmail.com>
2017-04-12 17:37:31 -0700

    Added the request Service and updated the home route to support asynchronous load of the page.

--- app/routes/home/home.component.ts
+++ app/routes/home/home.component.ts
@@ -1,29 +1,35 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { RequestService } from "../../RequestService/requests";
 
 @Component({
   selector: 'home',
   templateUrl: 'app/routes/home/home.component.html',
+  providers: [ RequestService ]
 })
 
-export class HomeComponent implements  OnInit {
+export class HomeComponent {
     query: string = "";
-    filtered: any[];
-    forms: any[] = [
-        {formID: "1", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
-        {formID: "2", name: "chef", img: "http://lorempixel.com/300/200/abstract/", desc: "cookin'"},
-        {formID: "3", name: "web wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "the best job"},
-        {formID: "4", name: "regular wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "another good one"},
-        {formID: "5", name: "president", img: "http://lorempixel.com/300/200/abstract/", desc: "of the US? or just ASWWU?"},
-        {formID: "6", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
-        {formID: "7", name: "chef", img: "http://lorempixel.com/300/200/abstract/", desc: "cookin'"},
-        {formID: "8", name: "web wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "the best job"},
-        {formID: "9", name: "regular wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "another good one"},
-        {formID: "10", name: "president", img: "http://lorempixel.com/300/200/abstract/", desc: "of the US? or just ASWWU?"},
-    ];
+    filtered: any[] = [];
+    forms: any[] = [];
 
-    ngOnInit() {
-        this.filtered = this.forms;
+    constructor( rs: RequestService) {
+      rs.get('/search/all', (data) => {
+        this.forms = [
+            {formID: "1", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
+            {formID: "2", name: "chef", img: "http://lorempixel.com/300/200/abstract/", desc: "cookin'"},
+            {formID: "3", name: "web wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "the best job"},
+            {formID: "4", name: "regular wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "another good one"},
+            {formID: "5", name: "president", img: "http://lorempixel.com/300/200/abstract/", desc: "of the US? or just ASWWU?"},
+            {formID: "6", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
+            {formID: "7", name: "chef", img: "http://lorempixel.com/300/200/abstract/", desc: "cookin'"},
+            {formID: "8", name: "web wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "the best job"},
+            {formID: "9", name: "regular wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "another good one"},
+            {formID: "10", name: "president", img: "http://lorempixel.com/300/200/abstract/", desc: "of the US? or just ASWWU?"},
+        ];
+        this.filterItems();
+      }, null);
     }
 
     filterItems() {
