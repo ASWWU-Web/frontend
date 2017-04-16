bbfa5c6eff1450ea431f2a0618a076c10505f559
asteroidice <ryan.s.rabello@gmail.com>
2017-04-16 12:13:27 -0700

    added search by department

--- app/routes/home/home.component.ts
+++ app/routes/home/home.component.ts
@@ -11,15 +11,16 @@ import { RequestService } from "../../RequestService/requests";
 
 export class HomeComponent {
     query: string = "";
+    department: string = "";
     filtered: any[] = [];
     forms: any[] = [];
 
     constructor(private rs: RequestService) {
       rs.get('/search/all', (data) => {
         this.forms = [
-            {formID: "1", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
-            {formID: "2", name: "chef", img: "http://lorempixel.com/300/200/abstract/", desc: "cookin'"},
-            {formID: "3", name: "web wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "the best job"},
+            {formID: "1", name: "dog whisperer - ASWWU Spiritual", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
+            {formID: "2", name: "chef - ASWWU Executive", img: "http://lorempixel.com/300/200/abstract/", desc: "cookin'"},
+            {formID: "3", name: "web wizard - ASWWU Web", img: "http://lorempixel.com/300/200/abstract/", desc: "the best job"},
             {formID: "4", name: "regular wizard", img: "http://lorempixel.com/300/200/abstract/", desc: "another good one"},
             {formID: "5", name: "president", img: "http://lorempixel.com/300/200/abstract/", desc: "of the US? or just ASWWU?"},
             {formID: "6", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs"},
@@ -33,9 +34,14 @@ export class HomeComponent {
     }
 
     filterItems() {
+      console.log("filterItems(", this.query, this.department, ")");
       this.filtered = this.forms.filter((el) => {
           var doc = el.name + el.desc;
          return doc.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
+      })
+      this.filtered = this.filtered.filter((el) => {
+          var doc = el.name + el.desc;
+         return doc.toLowerCase().indexOf(this.department.toLowerCase()) > -1;
       })
     }
 }
