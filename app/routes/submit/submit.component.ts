2b1abe57cd0bd74e4817456f5d9eb579143cc23b
Nolan Chinn <nolan694@gmail.com>
2017-04-21 16:32:21 -0700

    add login checking and generic form GETs

--- app/routes/submit/submit.component.ts
+++ app/routes/submit/submit.component.ts
@@ -1,53 +1,92 @@
 import {Component, NgModule} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router } from '@angular/router';
 
 import { RequestService } from "../../RequestService/requests";
 
 
 @Component({
-  selector: 'submit',
-  templateUrl: 'app/routes/submit/submit.component.html',
-  providers: [ RequestService ]
+	selector: 'submit',
+	templateUrl: 'app/routes/submit/submit.component.html',
+	providers: [ RequestService ]
 })
 
 export class SubmitComponent {
 	form: any;
+	gForm: any;
 	app: any;
+	gApp: any;
 	answers: any[] = [];
+	gAnswers:any[] = [];
 	formID: number;
+	currentUser: any;
 
-	constructor(private route: ActivatedRoute, private rs: RequestService) {
+	constructor(private route: ActivatedRoute, private rs: RequestService, private router: Router) {
 		this.formID = route.snapshot.params['formID'];	
-		//TODO, actually make some requests to the proper places //need to send formID
-		//GET request for form 
-		rs.get('/search/all', (data) => {
-			this.form = { formID: "1", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs", owner: "1", questions: [{ID: "1", text: "What's your favorite color?"}, {ID: "2", text: "What's the best animal?"}]};
-			rs.get('/search/all', (data) => {
-				this.app = { jobID: "1", answers: [ {questionID: "1", answer: "Roja"}, {questionID: "2", answer: "Dogs of course"}], username: "buddy.boy", status: "", last_update: ""};
-				if(!this.app) {
-					//build the empty answers array
-					this.form.questions.forEach((entry) => {
-						let answerObj = {questionID: entry.ID, answer: ""};
-						this.answers.push(answerObj);
-					});
-				} else {
-					this.app.answers.forEach((entry) => {
-						let answerObj = {questionID: entry.questionID, answer: entry.answer};
-						this.answers.push(answerObj);
-					});
-				}
-			}, undefined);
-		}, undefined);
+		rs.verify((user) => {
+			if(user) {
+				this.currentUser = user;
+				console.log(this.currentUser);
 
-		//GET request for application
-		//need to send jobID (formID), and username (have to get username)
-		
+				rs.get('/search/all', (data) => {
+					this.gForm = { formID: "1", name: "generic", img: "http://lorempixel.com/300/200/abstract/", desc: "baseline stuff", owner: "1", questions: [{ID: "1", text: "What does ASWWU mean to you?"}]};
+					//GET request to retrieve previous application answers
+					rs.get('/search/all', (data) => {
+						this.gApp = { jobID: "1", answers: [ {questionID: "1", answer: "many things"}], username: "buddy.boy", status: "", last_update: ""};
+						if(!this.gApp) {
+							//build the empty answers array
+							this.gForm.questions.forEach((entry) => {
+								let answerObj = {questionID: entry.ID, answer: ""};
+								this.gAnswers.push(answerObj);
+							});
+						} else {
+							this.gApp.answers.forEach((entry) => {
+								let answerObj = {questionID: entry.questionID, answer: entry.answer};
+								this.gAnswers.push(answerObj);
+							});
+						}
+					}, undefined);
+				}, undefined);
+
+				//GET request to retrieve the form
+				rs.get('/search/all', (data) => {
+					this.form = { formID: "1", name: "dog whisperer", img: "http://lorempixel.com/300/200/abstract/", desc: "talk to dogs", owner: "1", questions: [{ID: "1", text: "What's your favorite color?"}, {ID: "2", text: "What's the best animal?"}]};
+					//GET request to retrieve previous application answers
+					rs.get('/search/all', (data) => {
+						this.app = { jobID: "1", answers: [ {questionID: "1", answer: "Roja"}, {questionID: "2", answer: "Dogs of course"}], username: "buddy.boy", status: "", last_update: ""};
+						if(!this.app) {
+							//build the empty answers array
+							this.form.questions.forEach((entry) => {
+								let answerObj = {questionID: entry.ID, answer: ""};
+								this.answers.push(answerObj);
+							});
+						} else {
+							this.app.answers.forEach((entry) => {
+								let answerObj = {questionID: entry.questionID, answer: entry.answer};
+								this.answers.push(answerObj);
+							});
+						}
+					}, undefined);
+				}, undefined);
+			}
+		});
 	}
 
 	onSubmit() {
 		let submission = { jobID: this.formID, username: "ryan.rabello", answers: this.answers};
-		console.log(submission);
-		this.rs.postxwww('/forms/application/submit', submission, (data) => {console.log(data);}, undefined);
+		this.rs.postxwww('/forms/application/submit', submission, (data) => {
+			try {
+				if(data.error) {
+					window.alert("Error: "+data.error);
+				} else if(data.status == "submitted") {
+					this.router.navigateByUrl("/done/"+this.formID);
+				} else {
+					window.alert("form status: "+data.status);
+				}
+			}
+			catch(err) {
+				window.alert(err);
+			}
+		}, (error) => { window.alert(error)} );
 	}
 
 }
