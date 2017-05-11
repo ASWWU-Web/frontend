ecc2b2bdd0128049fdd9a47ba0461f0456ef60bf
Nolan Chinn <nolan694@gmail.com>
2017-05-10 17:02:08 -0700

    navbar styling, menu ordering

--- app/shared/navbar.component.ts
+++ app/shared/navbar.component.ts
@@ -37,6 +37,15 @@ export class NavbarComponent {
     }
   }
 
-  displayUserOptions(){
+  displayUserOptions(): void{
+  }
+
+  logout():void {
+    document.cookie="token=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    document.getElementById("bubble-popup").style.display = 'none';
+    this.currentUser = undefined;
+    this.rs.verify();
+    this.isLoggedIn = false;
+    this.isAdmin = false;
   }
 }
