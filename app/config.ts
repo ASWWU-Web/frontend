1a24077e2607efea522537ad4f921da0ec1da984
Ethan Beaver <ethan.beaver@ahss.org>
2018-04-16 15:51:20 -0700

    (WIP) - Edit page for jobs

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,8 @@
 //DOMAINS
-export var SERVER_URL = "https://aswwu.com/server";
-export var COOKIE_DOMAIN = ".aswwu.com";
-// export const SERVER_URL = "http://127.0.0.1:8888";
-// export const COOKIE_DOMAIN = ".localhost";
+// export var SERVER_URL = "https://aswwu.com/server";
+// export var COOKIE_DOMAIN = ".aswwu.com";
+export const SERVER_URL = "http://127.0.0.1:8888";
+export const COOKIE_DOMAIN = ".localhost";
 
 //IMAGES
 export const MEDIA_URI = 'https://aswwu.com/media';
@@ -13,5 +13,5 @@ export const MEDIA_LG = MEDIA_URI + "/img-lg";
 export const DEFAULT_PHOTO = 'images/mask_unknown.png';
 
 //YEARS
-export const CURRENT_YEAR = '1617';
-export const ARCHIVE_YEARS = ["1516","1415","1314","1213","1112","1011","0910","0809","0708","0607"];
+export const CURRENT_YEAR = '1718';
+export const ARCHIVE_YEARS = ["1617","1516","1415","1314","1213","1112","1011","0910","0809","0708","0607"];
--- app/routes/admin/edit/admin-edit.component.ts
+++ app/routes/admin/edit/admin-edit.component.ts
@@ -1,17 +1,68 @@
-import {Component, NgModule} from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { Router } from '@angular/router';
+
+import { RequestService } from "../../../RequestService/requests";
 
 @Component({
   selector: 'admin-edit',
-  template: `
-  <h1>Admin is editing form {{formID}}</h1>
-  `,
+  templateUrl: 'app/routes/admin/edit/admin-edit.component.html'
 })
 
-export class AdminEditComponent {
-	formID: number;
+export class AdminEditComponent implements OnInit {
+  formID: number;
+  jobName: string = "";
+  jobDesc: string = "";
+  visibility: number = 1;
+  owner: string = "";
+  imgLink: string = "";
+  questions: any[] = [{question: ""}];
+  department: string = "";
 
-	constructor(route: ActivatedRoute) {
+  constructor(private rs: RequestService, private router: Router, route: ActivatedRoute) {
 		this.formID = +route.snapshot.params['formID'];
-	}
+  }
+
+  ngOnInit() {
+    // TODO: Check to make sure the user is logged in. This isn't that important because this page is for admins.
+    // rs.verify((user) => {
+    //   if(user){
+    //     this.owner = user.username;
+    //   } else {
+    //     console.log('Navigating!!');
+    //     // this.router.navigateByUrl("/");
+    //   }
+    // });
+  }
+
+  addQuestion() {
+    this.questions.push({question: ""});
+  }
+  submitForm() {
+    this.rs.postxwww("/forms/job/new", {
+      job_name: this.jobName,
+      job_description: this.jobDesc,
+      visibility: this.visibility,
+      department: this.department,
+      owner: this.owner,
+      image: this.imgLink,
+      questions: this.questions
+
+    }, (data) => {
+      if(data.status == "submitted"){
+        this.jobName = "";
+        this.jobDesc = "";
+        this.visibility = 1;
+        this.owner = "";
+        this.imgLink = "";
+        this.questions = [{question: ""}];
+        this.department = "";
+      } else {
+        window.alert("An unknown error occured.");
+      }
+    }, (error) => {window.alert("ERROR: \n" + error) });
+  }
+  removeQuestion(index: number) {
+    this.questions.splice(index,1);
+  }
 }
