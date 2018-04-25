870839e8020604aaf96fbfc3227c0f9d5440f8c3
Ethan Beaver <ethan.beaver@ahss.org>
2018-04-25 15:20:31 -0700

    Adding edit link to admin page for jobs, improving styling

--- app/routes/admin/admin.component.ts
+++ app/routes/admin/admin.component.ts
@@ -12,15 +12,18 @@ import { RequestService } from '../../RequestService/requests';
     <div id="cards" class="row justify-content-center">
 
       <!-- Cards -->
-      <div *ngFor="let form of forms" class="col col-sm-12 col-md-6 col-lg-4 col-xl-3">
+      <div *ngFor="let form of forms" class="col col-sm-12 col-md-6 col-lg-4 col-xl-3 card-group">
         <div class="card">
         <img *ngIf="form.image != ''" class="card-img-top" src="{{form.image}}" alt="Card image cap">
-          <div class="card-block">
+          <div class="card-block d-flex flex-column">
             <h3 class="card-title">{{form.job_name}}</h3>
             <h6 class="card-subtitle mb-2 text-uppercase" style="color:#8f8f8f;">{{form.department}}</h6>
-      <p class="card-text">{{shorten(form.job_description)}}
-              </p>
-            <a [routerLink]="['/admin/review/', form.jobID]" class="btn">View Applications</a>
+            <p class="card-text">{{shorten(form.job_description)}}
+            </p>
+            <div class="btn-group mt-auto">
+              <a [routerLink]="['/admin/review/', form.jobID]" class="btn btn-secondary">Applicants</a>
+              <a [routerLink]="['/admin/edit/', form.jobID]" class="btn btn-secondary">Edit</a>
+            </div>
           </div>
         </div>
       </div>
