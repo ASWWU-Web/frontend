eda1712072d751a9a3cae1e46ffb32b3b70e5398
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-04-04 15:11:32 -0700

    add shared container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RequestService } from './RequestService/requests';
+import { RequestService as OLD_RS} from './RequestService/requests';
 
 import {
   HomeComponent,
@@ -27,8 +28,28 @@ import {
   FileSelectDirective
 } from 'ng2-file-upload';
 
+// shared-ng
+
+import {
+  SharedNgContainerComponent,
+  FooterComponent,
+  NavBarComponent,
+  MobileNavComponent,
+  UserBubbleComponent,
+  HeaderComponent
+} from '../shared-ng/components/components';
+import {
+  RequestService
+} from '../shared-ng/services/services';
+
 @NgModule({
   declarations: [
+    SharedNgContainerComponent,
+    FooterComponent,
+    NavBarComponent,
+    MobileNavComponent,
+    UserBubbleComponent,
+    HeaderComponent,
     AppComponent,
     HomeComponent,
     SubmitComponent,
@@ -46,11 +67,13 @@ import {
     BrowserModule,
     FormsModule,
     HttpModule,
+    HttpClientModule,
     NgbModule.forRoot(),
     AppRoutingModule,
   ],
   providers: [
     RequestService,
+    OLD_RS
   ],
   bootstrap: [AppComponent]
 })
