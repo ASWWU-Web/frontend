a56ab9ff86c8d8b3a14a62dfb71df1619c00bd18
Nick <nick97eagles@gmail.com>
2020-01-28 14:26:17 -0800

    adding error page to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { ErrorPageComponent } from '../shared-ng/components/error-page/error-page.component';
 
 
 const routes: Routes = [
