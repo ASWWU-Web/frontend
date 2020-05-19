6e0fd112611a82467b6ecfe2f495e0dcb72333ec
Michael Stacy <michael.stacy@wallawalla.edu>
2020-05-18 19:06:49 -0700

    created a request service to retrieve actual notifications from the db and display them based on whether they are visible or not

--- src/app/modules/homepage/components/notification/notification-view/notification-view.component.ts
+++ src/app/modules/homepage/components/notification/notification-view/notification-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Notification} from "../../../../../../shared-ng/interfaces/homepage";
+import {NotificationRequestService} from "../../../../../../shared-ng/services/notification.request.service";
 
 @Component({
   selector: 'notifications',
@@ -8,9 +9,7 @@ import {Notification} from "../../../../../../shared-ng/interfaces/homepage";
 })
 export class NotificationViewComponent implements OnInit {
 
-  constructor() { }
-
-  // notifications: Notification[];
+  constructor(private nrs: NotificationRequestService) { }
 
   severityOptions = {
     1: "alert-primary",
@@ -18,36 +17,40 @@ export class NotificationViewComponent implements OnInit {
     3: "alert-danger",
   }
 
+  notifications: Notification[];
 
-  notifications = [
-    {
-      notification_text: "stuff here",
-      notification_link: "google.com",
-      start_time: "2020-05-18 17:31:00.0",
-      end_time: "2020-05-19 17:31:00.0",
-      visibility: 1,
-      severity: 1,
-    },
-    {
-      notification_text: "stuff here 2.0",
-      notification_link: "abc.com",
-      start_time: "2020-05-18 17:31:00.0",
-      end_time: "2020-05-19 17:31:00.0",
-      visibility: 1,
-      severity: 2,
-    },
-    {
-      notification_text: "stuff here 3.0",
-      notification_link: "abc.com",
-      start_time: "2020-05-18 17:31:00.0",
-      end_time: "2020-05-19 17:31:00.0",
-      visibility: 1,
-      severity: 3,
-    },
-  ];
+//  notifications = [
+//    {
+//      notification_text: "stuff here",
+//      notification_link: "google.com",
+//      start_time: "2020-05-18 17:31:00.0",
+//      end_time: "2020-05-19 17:31:00.0",
+//      visibility: 1,
+//      severity: 1,
+//    },
+//    {
+//      notification_text: "stuff here 2.0",
+//      notification_link: "abc.com",
+//      start_time: "2020-05-18 17:31:00.0",
+//      end_time: "2020-05-19 17:31:00.0",
+//      visibility: 1,
+//      severity: 2,
+//    },
+//    {
+//      notification_text: "stuff here 3.0",
+//      notification_link: "abc.com",
+//      start_time: "2020-05-18 17:31:00.0",
+//      end_time: "2020-05-19 17:31:00.0",
+//      visibility: 1,
+//      severity: 3,
+//    },
+//  ];
 
 
   ngOnInit() {
+    this.nrs.listNotifications({visible: 1}).subscribe(data => {
+      this.notifications = data;
+    }, error => {}, () => {});
   }
 
 }
