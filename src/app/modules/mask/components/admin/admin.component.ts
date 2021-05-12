7e1fafdd0458d2820f69885b00f2fdb92756d8c2
caherbel <caherbel@gmail.com>
2021-05-12 15:49:08 -0700

    Upload mass photos should be close to wrapped up

--- src/app/modules/mask/components/admin/admin.component.ts
+++ src/app/modules/mask/components/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ElementRef, ChangeDetectorRef} from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {
   RequestService,
@@ -6,12 +6,10 @@ import {
   HermesService,
   MaskRequestService
 } from '../../../../../shared-ng/services/services';
-import {Observable, Subscription, BehaviorSubject} from 'rxjs';
+import { Subscription} from 'rxjs';
 import {User} from '../../../../../shared-ng/interfaces/interfaces';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {FileUploader} from 'ng2-file-upload';
 import {environment} from '../../../../../shared-ng/environments/environment';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   templateUrl: 'admin.component.html',
@@ -28,62 +26,61 @@ export class AdminComponent implements OnInit {
   images = [];
   csvinput = {};
   csvExists = false;
-
-  // Mathew
-  closeResult = '';
-  fileToUpload: File = null;
-  srcString: any = null;
-  profile: User;
-  // End Mathew
+  permissions = false;
+  hasError = true;
+  error = [];
 
   public uploader: FileUploader = new FileUploader({url: environment.SERVER_URL + '/forms/resume/upload'});
-
-  myForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    file: new FormControl('', [Validators.required]),
-    fileSource: new FormControl('', [Validators.required])
-  });
-
   constructor(private route: ActivatedRoute,
               private as: AuthService, private hs: HermesService, private elementRef: ElementRef,
-              private mrs: MaskRequestService, private modalService: NgbModal) {
+              private mrs: MaskRequestService) {
     // sets background color
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'black';
   }
 
   ngOnInit() {
     this.buildLoginLink = this.as.buildLoginLink;
-    this.currentUser = {
-      full_name: 'Caleb Herbel',
-      photo: 'test',
-      roles: 'mask-admin',
-      status: 'true',
-      username: 'caleb.herbel',
-      wwuid: '2022977'
-    };
-    // this.userInfoSubscription = this.as.getUserInfo().subscribe(
-    //   (data: User) => {
-    //     this.currentUser = data;
-    //   }
-    // );
+    this.userInfoSubscription = this.as.getUserInfo().subscribe(
+      (data: User) => {
+        this.currentUser = data;
+      }
+    );
+    this.setPermissions();
   }
 
-  get f() {
-    return this.myForm.controls;
+  public setPermissions() {
+    this.mrs.post('/verify/mask-permissions', {}).subscribe(
+      (data: boolean) => {
+          this.permissions = data['permission'];
+      }, () => {
+        this.permissions = false;
+      });
   }
 
   public csvToDict(csv) {
     const lines = csv.split('\n');
     const result = {};
-    const headers = lines[0].split(',');
 
     for (let i = 1; i < lines.length; i++) {
       const currentline = lines[i].split(',');
       const photoLine = currentline[1].split(' ');
+      this.hasError = false;
       photoLine.map(x => {
         x.replace(/(\r\n|\n|\r)/gm, '');
-        if (!Number.isInteger(x)) {
-          x = parseInt(x, 10);
+        const currentPhoto = x;
+        if (!/\S/.test(x) || x === '') {
+          // Do Nothing empty spaces
+        } else if (!Number.isInteger(x)) {
+          try {
+            if (isNaN(parseInt(x, 10)) || parseFloat(x) % 1 !== 0) {
+              throw new Error('Not Whole Number');
+            } else {
+              x = parseInt(x, 10);
+            }
+          } catch (e) {
+            this.error.push('Photo: ' + currentPhoto + ' is currently failing.');
+            this.hasError = true;
+          }
         }
         result[x] = currentline[0];
       });
@@ -96,7 +93,6 @@ export class AdminComponent implements OnInit {
     this.csvinput = {};
     this.csvExists = false;
 
-    const input = document.getElementById('fileInput');
     const reader = new FileReader();
     reader.onload = () => {
       this.csvinput = this.csvToDict(reader.result);
@@ -122,49 +118,16 @@ export class AdminComponent implements OnInit {
             studentID: this.csvinput[photoNumber[0]],
             uploaded: false
           });
-          this.myForm.patchValue({
-            fileSource: this.images
-          });
         };
         reader.readAsDataURL(file);
       }
     }
-  }
-
-  handleFileInput(event: any): void {
-    console.log(event.target.files);
-    if (event.target.files && event.target.files[0]) {
-      for (let i = 0; i < event.target.files.length; i++) {
-        const file = event.target.files[i];
-
-        const reader = new FileReader();
-
-        reader.onload = e => {
-          this.srcString = reader.result;
-          this.fileToUpload = event.target.files.item(0);
-        };
-
-        reader.readAsDataURL(file);
-      }
-
-    }
-  }
-
-  // Temporary
-  async delay(ms: number) {
-    await new Promise(resolve => setTimeout(() => resolve(), ms));
   }
 
   async submit() {
-    console.log('Finished');
     for (const i of Object.keys(this.images)) {
-      await this.postFile(this.images[i].photo, i, this.images[i].studentID).then(() => {
-        const newImages = this.images;
-        newImages[i].uploaded = true;
-        this.images = newImages;
-      });
+      await this.postFile(this.images[i].photo, i, this.images[i].studentID);
     }
-    console.log(this.images);
   }
 
   async postFile(fileToUpload: File, photoID: string, studentID: string) {
@@ -172,9 +135,12 @@ export class AdminComponent implements OnInit {
     const name = `${d.getDay()}-${d.getMonth()}-${d.getFullYear()}-ID${d.getMinutes()}${d.getSeconds()}${photoID}_${studentID}.jpeg`;
     const $uploadPhoto = await this.mrs.uploadProfilePhotoDirect(fileToUpload, name);
     $uploadPhoto.subscribe({
-      error: (err) => {
-        console.log('Err');
+      next: () => {
+        const newImages = this.images;
+        newImages[photoID].uploaded = true;
+        this.images = newImages;
       },
+      error: () => console.log('Error'),
       complete: () => console.log('SUCCESS')
     });
   }
