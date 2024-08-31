4adbce719d371c95bbd676dc0505c50b5300adc9
Riley Smith <github@rileyy.dev>
2024-08-30 23:16:42 -0700

    chore: update linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-  
+
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
@@ -23,7 +23,7 @@ module.exports = tseslint.config(
         },
       ],
       "@angular-eslint/component-selector": [
-        "error",
+        "warn",
         {
           type: "element",
           prefix: "app",
